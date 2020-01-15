6f04400a83c9989909fe446ea8a922058d180b4d
Md Jahidul Islam <jahidul@Mds-MBP-2.fios-router.home>
2020-01-14 21:22:01 -0500

    added extra class

---
+++ automationpractice/src/test/java/page_objects_test/ContactUsPageTest.java
@@ -0,0 +1,11 @@
+package page_objects_test;
+
+import base.BrowserDriver;
+import org.testng.annotations.Test;
+
+public class ContactUsPageTest extends BrowserDriver {
+    @Test
+    public void demoTest(){
+        System.out.println("HI");
+    }
+}
